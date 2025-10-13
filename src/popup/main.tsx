5acb9a7a91d2c84fe6e4ddb4fd63f3cbb045e676
AughtDev <aughtdev@gmail.com>
2025-10-13 08:03:44 +0300

    git init

---
+++ src/popup/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+
+
+// Find the root DOM element where the React app will be mounted.
+const container = document.getElementById('root');
+
+console.log('Mounting React app to container:', container);
+// Ensure the container exists before attempting to create a root.
+if (container) {
+    // Create a React root and render the App component.
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+                <App/>
+        </React.StrictMode>
+    );
+} else {
+    console.error('Root element not found. Cannot mount React app.');
+}
